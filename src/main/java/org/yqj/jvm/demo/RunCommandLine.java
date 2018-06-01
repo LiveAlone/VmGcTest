8e985d9b6e18ef13413326ec36caaffea96107d6
yaoqijun <yaoqijun@changingedu.com>
2018-06-01 14:02:55 +0800

    init

---
+++ src/main/java/org/yqj/jvm/demo/RunCommandLine.java
@@ -0,0 +1,25 @@
+package org.yqj.boot.demo;
+
+import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+
+/**
+ * Created by yaoqijun.
+ * Date:2016-04-27
+ * Email:yaoqj@terminus.io
+ * Descirbe:
+ */
+@Component
+@Slf4j
+public class RunCommandLine implements CommandLineRunner{
+
+    public void run(String... args) throws Exception {
+        log.info(" command info run");
+        System.out.println("now command line run ");
+        System.out.println("args content is " + Lists.newArrayList(args).toString());
+    }
+}
